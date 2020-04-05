d062191851074fcec95f58b84f7bfcdcf98f6414
Uma Nagaswamy <swamyuma@users.noreply.github.com>
2020-04-05 12:58:52 -0500

    Fix variable naming
    Swap quotient and remainder

--- dynamic-programming/hamming-distance.py
+++ dynamic-programming/hamming-distance.py
@@ -3,24 +3,25 @@ class Solution:
     '''
     https://en.wikipedia.org/wiki/Hamming_distance
     '''
-    def decimal_to_binary(self, num, quotient_array):
+    def decimal_to_binary(self, num, remainder_array):
         
         # raise error
         if num < 0:
             raise ValueError("Input should be an unsigned integer!")
     
         # calcuate
-        remainder = num//2
-        quotient = num%2    
+        quotient = num//2
+        remainder = num%2    
         
         if num > 1:
             # recursion
-            self.decimal_to_binary(remainder, quotient_array)
+            self.decimal_to_binary(quotient, remainder_array)
         
-        # accumalte quotient in an array
-        quotient_array.append(quotient)
-        #return quotient_array
-        return "".join([str(ele) for ele in quotient_array])
+        # accumalte remainder in an array
+        remainder_array.append(remainder)
+        
+        #return remainder_array
+        return "".join([str(ele) for ele in remainder_array])
         
     def hammingDistance(self, x: int, y: int) -> int:
         
