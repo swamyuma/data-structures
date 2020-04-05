84dfee79d0bc0911c1db953f2835a0830f48a912
Uma Nagaswamy <swamyuma@users.noreply.github.com>
2020-04-05 12:50:30 -0500

    Add methodology

--- dynamic-programming/hamming-distance.py
+++ dynamic-programming/hamming-distance.py
@@ -36,3 +36,26 @@ class Solution:
             if binary_x[ii] != binary_y[ii]:
                 num_of_diffs += 1
         return num_of_diffs
+
+## Leetcode
+'''
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different.
+'''
